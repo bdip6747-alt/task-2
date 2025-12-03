e86249106cd9883a9ebe73429e9976f057c092df
bdip6747-alt <bdip6747@gmail.com>
2025-12-03 01:51:24 -0800

    Add files via upload
    Assiment2

---
+++ 001.py
@@ -0,0 +1,12 @@
+# using if eals
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even Number")
+else :
+    print("Odd Number")
+
+# sum 1 to 50 using loop
+total = 0
+for i in range(1, number):
+    total += i
+    print(total)
